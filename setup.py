f1785c4463dcf3a541cabee3de57fd5355d78970
Mark <Mark.Hall@work.room3b.eu>
2018-07-03 21:49:24 +0200

    Version 1.1.2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ tests_require = [
 
 setup(
     name='museum-map',
-    version='1.1.1',
+    version='1.1.2',
     description='Code for generating the thematic museum map',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
