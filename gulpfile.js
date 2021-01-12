c56d81adbfcb8038c0d76abf567101b1b23a9c8a
Mark Hall <mark.hall@work.room3b.eu>
2021-01-12 13:50:04 +0000

    Update dependencies and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,20 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var pump = require('pump');
-var concat = require('gulp-concat');
+const gulp = require('gulp'),
+      { spawn } = require('child_process');
 
-gulp.task('scss', function(cb) {
-    pump([
-        gulp.src('src/frontend/scss/application.scss'),
-        sass({
-            includePaths: [
-                'node_modules/foundation-sites/scss',
-                'node_modules/foundation-sites/_vendor'
-            ]
-        }),
-        gulp.dest('src/museum_map/static')
-    ], cb);
+gulp.task('develop', (cb) => {
+    const builder = spawn('yarn', ['build', '--mode', 'development', '--watch', '--dest', '../museum_map/static'], {
+        cwd: 'src/frontend',
+        stdio: 'inherit',
+    });
+    builder.on('exit', cb);
 });
 
-gulp.task('js', function(cb) {
-    pump([
-        gulp.src([
-            'node_modules/jquery/dist/jquery.js',
-            'node_modules/foundation-sites/dist/js/foundation.js',
-            'node_modules/easeljs/lib/easeljs.js',
-            'node_modules/gdpr-tracking-client/index.js',
-            'src/frontend/js/*.js'
-        ]),
-        concat('application.js'),
-        gulp.dest('src/museum_map/static')
-    ], cb);
+gulp.task('default', (cb) => {
+    const builder = spawn('yarn', ['build', '--mode', 'production', '--dest', '../museum_map/static'], {
+        cwd: 'src/frontend',
+        stdio: 'inherit',
+    });
+    builder.on('exit', cb);
 });
-
-gulp.task('default', gulp.parallel('scss', 'js'));
-
-// Task to watch the SCSS/JS files and re-build when needed
-gulp.task('watch', gulp.series('default', function() {
-    gulp.watch('src/frontend/scss/**/*.scss', ['scss']);
-    gulp.watch('src/frontend/js/**/*.js', ['js']);
-}));
