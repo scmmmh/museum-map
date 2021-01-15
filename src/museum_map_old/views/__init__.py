9f9708677c67099d5bce215c4e8e05278c216864
Mark Hall <mark.hall@work.room3b.eu>
2021-01-15 17:46:15 +0000

    Remove old code

--- src/museum_map_old/views/__init__.py
+++
@@ -1,9 +0,0 @@
-from pyramid.httpexceptions import HTTPFound
-from pyramid.view import view_config
-
-from ..models import Group
-
-@view_config(route_name='root')
-def root(request):
-    group = request.dbsession.query(Group).filter(Group.parent_id == None).first()
-    raise HTTPFound(location=request.route_url('explore', gid=group.id))
