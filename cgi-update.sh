0f9cddcd340c0c1d694b73c5bca9606c1abe955e
Mark Hall <mark.hall@work.room3b.eu>
2021-07-26 09:25:12 +0100

    Cleanup

--- cgi-update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Content-Type: text/text"
-echo ""
-echo "Update started"
-./update.sh 1>./build.log 2>./build-error.log &
--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-hg pull --update
-export PIPENV_VENV_IN_PROJECT=True
-pipenv install
-pipenv run pip install .
