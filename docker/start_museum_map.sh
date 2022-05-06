4c5e4498f42244cb5465012964652a2330c62ebc
Mark Hall <mark.hall@work.room3b.eu>
2022-05-06 11:46:48 +0100

    Fix the start script

--- docker/start_museum_map.sh
+++ docker/start_museum_map.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-DEFAULT_GATEWAY=`ip route show | grep "default via" | grep -E -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]"`
-
 # Run the web application
 python -m museum_map -c /etc/museum-map/production.ini server run --port 80
