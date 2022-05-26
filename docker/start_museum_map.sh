b33bac38e97dcafaeb2299aaab812b1c80adcee8
Mark Hall <mark.hall@work.room3b.eu>
2022-05-26 13:03:01 +0100

    Fix a bug in the startup script

--- docker/start_museum_map.sh
+++ docker/start_museum_map.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the web application
-python -m museum_map -c /etc/museum-map/production.ini -v server run --port 8080
+python -m museum_map -c /etc/museum-map/production.yml -v server run
