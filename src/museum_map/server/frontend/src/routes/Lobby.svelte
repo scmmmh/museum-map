0d85799aaf3b9223425749d0e8fef8fc5e8f1799
Mark Hall <mark.hall@work.room3b.eu>
2022-05-27 12:45:21 +0100

    A11y fix

--- src/museum_map/server/frontend/src/routes/Lobby.svelte
+++ src/museum_map/server/frontend/src/routes/Lobby.svelte
@@ -58,8 +58,8 @@
         <section class="col-span-12">
             <div class="flex flex-row">
                 <h2 class="flex-1 text-xl font-bold mb-4">Selection from our collections</h2>
-                <button on:click={() => { fetchRandomItemsSelection(); }} class="flex-none px-2 py-2">
-                    <svg viewBox="0 0 24 24" class="w-6 h-6 fill-white">
+                <button on:click={() => { fetchRandomItemsSelection(); }} class="flex-none px-2 py-2" aria-label="Update the selection of items from the collection">
+                    <svg viewBox="0 0 24 24" class="w-6 h-6 fill-white" aria-hidden="true">
                         <path d="M2 12C2 16.97 6.03 21 11 21C13.39 21 15.68 20.06 17.4 18.4L15.9 16.9C14.63 18.25 12.86 19 11 19C4.76 19 1.64 11.46 6.05 7.05C10.46 2.64 18 5.77 18 12H15L19 16H19.1L23 12H20C20 7.03 15.97 3 11 3C6.03 3 2 7.03 2 12Z"></path>
                     </svg>
                 </button>
