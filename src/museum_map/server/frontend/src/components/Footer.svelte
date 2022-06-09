da2d3280ad346646701515a3a6a8af51aa715081
Mark Hall <mark.hall@work.room3b.eu>
2022-06-09 19:24:33 +0100

    Improve footer link UX

--- src/museum_map/server/frontend/src/components/Footer.svelte
+++ src/museum_map/server/frontend/src/components/Footer.svelte
@@ -6,12 +6,12 @@
     <nav>
         <ul class="flex flex-row flex-wrap gap-y-1">
             <li role="presentation" class="flex-none w-full md:w-1/3 px-2 text-center md:text-left">
-                <a href="https://www.room3b.eu/pages/projects/digital-museum-map.html" target="_blank" rel="noopener" class="hover:underline focus:underline">Find out more about how this works</a>
+                <a href="https://www.room3b.eu/pages/projects/digital-museum-map.html" target="_blank" rel="noopener" class="underline decoration-dotted hover:decoration-solid focus:decoration-solid">Find out more about how this works</a>
             </li>
             <li role="presentation" class="flex-none w-full md:w-1/3 pt-1 md:pt-0 md:py-0text-center">
                 {#if $config && $config.attributes.footer && $config.attributes.footer.center}
                     {#if $config.attributes.footer.center.url}
-                        <a href="{$config.attributes.footer.center.url}" target="_blank" rel="noopener" class="hover:underline focus:underline">{@html $config.attributes.footer.center.label}</a>
+                        <a href="{$config.attributes.footer.center.url}" target="_blank" rel="noopener" class="underline decoration-dotted hover:decoration-solid focus:decoration-solid">{@html $config.attributes.footer.center.label}</a>
                     {:else}
                         <span>{@html $config.attributes.footer.center.label}</span>
                     {/if}
@@ -20,7 +20,7 @@
             <li role="presentation" class="flex-none w-full md:w-1/3 px-2 pt-1 md:pt-0 text-center md:text-right">
                 {#if $config && $config.attributes.footer && $config.attributes.footer.right}
                     {#if $config.attributes.footer.right.url}
-                    <a href="{$config.attributes.footer.right.url}" target="_blank" rel="noopener" class="hover:underline focus:underline">{@html $config.attributes.footer.right.label}</a>
+                    <a href="{$config.attributes.footer.right.url}" target="_blank" rel="noopener" class="underline decoration-dotted hover:decoration-solid focus:decoration-solid">{@html $config.attributes.footer.right.label}</a>
                 {:else}
                     <span>{@html $config.attributes.footer.right.label}</span>
                 {/if}
