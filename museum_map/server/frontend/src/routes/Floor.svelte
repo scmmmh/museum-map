221fb99a8a6bf15452a7a59500ba9ddd9ed38851
Mark Hall <mark.hall@work.room3b.eu>
2022-12-01 11:41:08 +0000

    Progress on the new map display

--- museum_map/server/frontend/src/routes/Floor.svelte
+++ museum_map/server/frontend/src/routes/Floor.svelte
@@ -7,10 +7,15 @@
     import Header from '../components/Header.svelte';
     import Footer from '../components/Footer.svelte';
     import Thumnail from '../components/Thumbnail.svelte';
-    import { floors, loadRooms, busyCounter } from '../store';
+    import { floors, loadRooms, busyCounter, localPreferences } from '../store';
+    import type { NestedStorage } from '../store/preferences';
 
     const navigate = useNavigate();
     const params = useParams();
+
+    const MODE_MAP = 1;
+    const MODE_LIST = 2;
+    let mode = ($localPreferences.ui && ($localPreferences.ui as NestedStorage).floorDisplayMode) ? ($localPreferences.ui as NestedStorage).floorDisplayMode : MODE_MAP;
 
     type MapObject = {
         position: { x: number, y: number, width: number, height: number },
@@ -151,6 +156,11 @@
                 this.cameraPosition.y = this.cameraPosition.y + (newHeight - baseHeight) * ((pointer.y + this.cameraPosition.y) / this.baseMap.displayHeight);
                 this.scaleObjects(false);
             });
+
+            this.scale.on('resize', () => {
+                this.zoom = Math.min(this.game.canvas.width / this.baseMap.width * 0.9, this.game.canvas.height / this.baseMap.height * 0.9);
+                this.scaleObjects(true);
+            });
         }
 
         scaleObjects(center: boolean) {
@@ -274,12 +284,35 @@
         }
     });
 
+    const rooms = derived(currentFloor, (currentFloor, set) => {
+        if (currentFloor) {
+            loadRooms((currentFloor.relationships.rooms.data as JsonApiObjectReference[]).map((ref) => { return ref.id; })).then((rooms) => {
+                set(rooms);
+            })
+        } else {
+            set([]);
+        }
+    }, [] as JsonApiObject[]);
+
     onDestroy(currentFloorUnsubscribe);
+
+    async function changeMode(newMode: number) {
+        mode = newMode;
+        localPreferences.setPreference('ui.floorDisplayMode', mode);
+        if (mode === MODE_MAP) {
+            await tick();
+        }
+    }
 </script>
 
-<div class="flex flex-col h-screen">
+{#if mode === MODE_LIST}
     <Header title="{$currentFloor ? $currentFloor.attributes.label : 'Loading...'}" nav={[{label: $currentFloor ? $currentFloor.attributes.label : 'Loading...', path: '/floor/' + ($currentFloor ? $currentFloor.id : '0')}]}/>
-    <div class="flex-none flex flex-row">
+{/if}
+<div class="flex flex-col {mode === MODE_MAP ? 'h-screen' : ''}">
+    {#if mode === MODE_MAP}
+        <Header title="{$currentFloor ? $currentFloor.attributes.label : 'Loading...'}" nav={[{label: $currentFloor ? $currentFloor.attributes.label : 'Loading...', path: '/floor/' + ($currentFloor ? $currentFloor.id : '0')}]}/>
+    {/if}
+    <div class="flex-none flex flex-row sticky top-24">
         <div class="flex-none ml-2 lg:ml-4 mt-2 lg:mt-4">
             {#if $previousFloor}
                 <Link to="/floor/{$previousFloor.id}" class="inline-block bg-neutral-600 px-4 py-3 lg:py-2 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800">⇧ {$previousFloor.attributes.label}</Link>
@@ -289,22 +322,29 @@
         </div>
         <div class="flex-1"></div>
         <div class="flex-none mr-2 lg:mr-4 mt-2 lg:mt-4">
-            <button class="inline-block bg-neutral-600 px-3 py-3 lg:py-3 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800">
+            <button on:click={() => { changeMode(MODE_MAP); }} class="inline-block bg-neutral-600 px-3 py-3 lg:py-3 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800 {mode === MODE_MAP ? 'bg-blue-800' : ''}">
                 <svg class="w-6 h-6" viewBox="0 0 24 24">
                     <path fill="currentColor" d="M15,19L9,16.89V5L15,7.11M20.5,3C20.44,3 20.39,3 20.34,3L15,5.1L9,3L3.36,4.9C3.15,4.97 3,5.15 3,5.38V20.5A0.5,0.5 0 0,0 3.5,21C3.55,21 3.61,21 3.66,20.97L9,18.9L15,21L20.64,19.1C20.85,19 21,18.85 21,18.62V3.5A0.5,0.5 0 0,0 20.5,3Z" />
                 </svg>
             </button>
         </div>
         <div class="flex-none mr-2 lg:mr-4 mt-2 lg:mt-4">
-            <button class="inline-block bg-neutral-600 px-3 py-3 lg:py-3 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800">
+            <button on:click={() => { changeMode(MODE_LIST); }} class="inline-block bg-neutral-600 px-3 py-3 lg:py-3 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800 {mode === MODE_LIST ? 'bg-blue-800' : ''}">
                 <svg class="w-6 h-6" viewBox="0 0 24 24">
                     <path fill="currentColor" d="M7,5H21V7H7V5M7,13V11H21V13H7M4,4.5A1.5,1.5 0 0,1 5.5,6A1.5,1.5 0 0,1 4,7.5A1.5,1.5 0 0,1 2.5,6A1.5,1.5 0 0,1 4,4.5M4,10.5A1.5,1.5 0 0,1 5.5,12A1.5,1.5 0 0,1 4,13.5A1.5,1.5 0 0,1 2.5,12A1.5,1.5 0 0,1 4,10.5M7,19V17H21V19H7M4,16.5A1.5,1.5 0 0,1 5.5,18A1.5,1.5 0 0,1 4,19.5A1.5,1.5 0 0,1 2.5,18A1.5,1.5 0 0,1 4,16.5Z" />
                 </svg>
             </button>
         </div>
     </div>
-    <article class="flex-1">
-        <div id="game" class="w-full h-full"></div>
+    <article class="flex-1 overflow-hidden">
+        <div id="game" class="w-full h-full {mode === MODE_MAP ? '' : 'hidden'}" aria-hidden="true"></div>
+        <div class="{mode === MODE_LIST ? '' : 'hidden'}">
+            <ol class="px-4 pb-6 pt-2">
+                {#each $rooms as room}
+                    <li><Link to="/room/{room.id}" class="block py-2 hover:underline focus:underline">{room.attributes.label}</Link></li>
+                {/each}
+            </ol>
+        </div>
     </article>
     <div class="flex-none">
         {#if $nextFloor}
