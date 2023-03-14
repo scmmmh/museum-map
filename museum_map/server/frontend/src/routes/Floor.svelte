7fd6b61a2ef0b93f7c718d1b6786d1ce1e23b6a1
Mark Hall <mark.hall@work.room3b.eu>
2023-03-14 15:12:35 +0100

    Small UX improvement

--- museum_map/server/frontend/src/routes/Floor.svelte
+++ museum_map/server/frontend/src/routes/Floor.svelte
@@ -430,14 +430,14 @@
         </div>
         <div class="flex-1"></div>
         <div class="flex-none mr-2 lg:mr-4 mt-2 lg:mt-4">
-            <button on:click={() => { changeMode(MODE_MAP); }} class="inline-block bg-neutral-600 px-3 py-3 lg:py-3 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800 {mode === MODE_MAP ? 'bg-blue-800' : ''}" aria-label="Explore the museum's map">
+            <button on:click={() => { changeMode(MODE_MAP); }} class="inline-block bg-neutral-600 px-3 py-3 lg:py-3 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800 {mode === MODE_MAP ? 'bg-blue-600' : ''}" aria-label="Explore the museum's map">
                 <svg class="w-6 h-6" viewBox="0 0 24 24" aria-hidden="true">
                     <path fill="currentColor" d="M15,19L9,16.89V5L15,7.11M20.5,3C20.44,3 20.39,3 20.34,3L15,5.1L9,3L3.36,4.9C3.15,4.97 3,5.15 3,5.38V20.5A0.5,0.5 0 0,0 3.5,21C3.55,21 3.61,21 3.66,20.97L9,18.9L15,21L20.64,19.1C20.85,19 21,18.85 21,18.62V3.5A0.5,0.5 0 0,0 20.5,3Z" />
                 </svg>
             </button>
         </div>
         <div class="flex-none mr-2 lg:mr-4 mt-2 lg:mt-4">
-            <button on:click={() => { changeMode(MODE_LIST); }} class="inline-block bg-neutral-600 px-3 py-3 lg:py-3 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800 {mode === MODE_LIST ? 'bg-blue-800' : ''}" aria-label="Explore the museum in text form">
+            <button on:click={() => { changeMode(MODE_LIST); }} class="inline-block bg-neutral-600 px-3 py-3 lg:py-3 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800 {mode === MODE_LIST ? 'bg-blue-600' : ''}" aria-label="Explore the museum in text form">
                 <svg class="w-6 h-6" viewBox="0 0 24 24" aria-hidden="true">
                     <path fill="currentColor" d="M7,5H21V7H7V5M7,13V11H21V13H7M4,4.5A1.5,1.5 0 0,1 5.5,6A1.5,1.5 0 0,1 4,7.5A1.5,1.5 0 0,1 2.5,6A1.5,1.5 0 0,1 4,4.5M4,10.5A1.5,1.5 0 0,1 5.5,12A1.5,1.5 0 0,1 4,13.5A1.5,1.5 0 0,1 2.5,12A1.5,1.5 0 0,1 4,10.5M7,19V17H21V19H7M4,16.5A1.5,1.5 0 0,1 5.5,18A1.5,1.5 0 0,1 4,19.5A1.5,1.5 0 0,1 2.5,18A1.5,1.5 0 0,1 4,16.5Z" />
                 </svg>
@@ -466,14 +466,14 @@
         <article id="content" class="flex-1 overflow-hidden relative">
             <nav class="hidden lg:flex flex-row absolute right-0 top-0">
                 <div class="flex-none mr-2 lg:mr-4 mt-2 lg:mt-4">
-                    <button on:click={() => { changeMode(MODE_MAP); }} class="inline-block bg-neutral-600 px-3 py-3 lg:py-3 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800 {mode === MODE_MAP ? 'bg-blue-800' : ''}" aria-label="Explore the museum's map">
+                    <button on:click={() => { changeMode(MODE_MAP); }} class="inline-block bg-neutral-600 px-3 py-3 lg:py-3 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800 {mode === MODE_MAP ? 'bg-blue-600' : ''}" aria-label="Explore the museum's map">
                         <svg class="w-6 h-6" viewBox="0 0 24 24" aria-hidden="true">
                             <path fill="currentColor" d="M15,19L9,16.89V5L15,7.11M20.5,3C20.44,3 20.39,3 20.34,3L15,5.1L9,3L3.36,4.9C3.15,4.97 3,5.15 3,5.38V20.5A0.5,0.5 0 0,0 3.5,21C3.55,21 3.61,21 3.66,20.97L9,18.9L15,21L20.64,19.1C20.85,19 21,18.85 21,18.62V3.5A0.5,0.5 0 0,0 20.5,3Z" />
                         </svg>
                     </button>
                 </div>
                 <div class="flex-none mr-2 lg:mr-4 mt-2 lg:mt-4">
-                    <button on:click={() => { changeMode(MODE_LIST); }} class="inline-block bg-neutral-600 px-3 py-3 lg:py-3 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800 {mode === MODE_LIST ? 'bg-blue-800' : ''}" aria-label="Explore the museum in text form">
+                    <button on:click={() => { changeMode(MODE_LIST); }} class="inline-block bg-neutral-600 px-3 py-3 lg:py-3 rounded-lg lg:underline-offset-2 lg:hover:bg-blue-800 lg:focus:bg-blue-800 {mode === MODE_LIST ? 'bg-blue-600' : ''}" aria-label="Explore the museum in text form">
                         <svg class="w-6 h-6" viewBox="0 0 24 24" aria-hidden="true">
                             <path fill="currentColor" d="M7,5H21V7H7V5M7,13V11H21V13H7M4,4.5A1.5,1.5 0 0,1 5.5,6A1.5,1.5 0 0,1 4,7.5A1.5,1.5 0 0,1 2.5,6A1.5,1.5 0 0,1 4,4.5M4,10.5A1.5,1.5 0 0,1 5.5,12A1.5,1.5 0 0,1 4,13.5A1.5,1.5 0 0,1 2.5,12A1.5,1.5 0 0,1 4,10.5M7,19V17H21V19H7M4,16.5A1.5,1.5 0 0,1 5.5,18A1.5,1.5 0 0,1 4,19.5A1.5,1.5 0 0,1 2.5,18A1.5,1.5 0 0,1 4,16.5Z" />
                         </svg>
