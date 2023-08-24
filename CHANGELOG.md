c1a3ff25410dbf716e1133f2be0baf29fc4c62ae
Mark <mark.hall@work.room3b.eu>
2023-08-24 16:58:34 +0100

    Add update handling (#55)

---
+++ CHANGELOG.md
@@ -0,0 +1,74 @@
+# Changelog
+
+## Dev
+
+* **New**: Add an indicator when a frontend reload is required
+* **Chore**: Update all dependencies
+* **Chore**: Switch to using hatch
+
+## 0.8.2
+
+* **Bugfix**: Fix security issues in dependencies
+
+## 0.8.1
+
+* **Bugfix**: Fixed a security issue in Tornado
+
+## 0.8.0
+
+* **Update**: Updated the search library
+* **Bugfix**: Fixed an issue with navigating to the lobby after searching
+* **Bugfix**: Fixed a layout issue with the heading
+
+## 0.7.3
+
+* **New**: Implemented search
+* **Bugfix**: Fixed an a11y issue in the floor display
+
+## 0.7.1
+
+* **Bugfix**: Fixed an issue with the floor previews positioning
+
+## 0.7.0
+
+* **Update**: Added floor previews
+* **Update**: Improved the floor browsing UI
+* **Bugfix**: Fixed an issue with the footer layout
+
+## 0.6.0
+
+* **Update**: Rewrite of the floor browsing UI
+
+## 0.2.6
+
+* **Update**: Performance improvements
+
+## 0.2.5
+
+* **Bugfix**: Fix dependency versions
+
+## 0.2.4
+
+* **Bugfix**: Fix an issue with the release preparation
+
+## 0.2.3
+
+* **Update**: Performance improvements
+
+## 0.2.2
+
+* **Update**: Work from python 3.9
+* **Bugfix**: Fixed issues required for asyncpg
+
+## 0.2.1
+
+* **Bugfix**: Deploy the production code
+
+## 0.2.0
+
+* **New**: Floor navigation
+* **Update**: Improved lobby
+
+## 0.1.0
+
+* **New**: Initial version
--- prepare_release.py
+++ prepare_release.py
@@ -1,29 +1,39 @@
+"""Script to prepare the version information for a release."""
 import re
+from typing import List
 
-
-VERSION = '0.8.2'
+VERSION = "0.8.2"
 
 
 def readlines(filename: str) -> list[str]:
+    """Read a file as a list of lines."""
     with open(filename) as in_f:
         return in_f.readlines()
 
 
-def writelines(filename: str, lines: list[str]):
-    with open(filename, 'w') as out_f:
-        out_f.write(''.join(lines))
+def writelines(filename: str, lines: list[str]) -> None:
+    """Write a list of lines to a file."""
+    with open(filename, "w") as out_f:
+        out_f.write("".join(lines))
 
 
-def update_version(filename: str, pattern: str, version: str):
+def update_version(filename: str, pattern: str, version: str) -> None:
+    """Update the version in a file."""
+
     def replace_version(line: str) -> str:
         if re.match(pattern, line):
-            return re.sub('[0-9]+\.[0-9]+\.[0-9]+', version, line)
+            return re.sub(pattern, fr"\g<1>{version}\g<2>", line)
         else:
             return line
 
     writelines(filename, map(replace_version, readlines(filename)))
 
 
-update_version('museum_map/server/frontend/package.json', '^  "version": "[0-9]+\.[0-9]+\.[0-9]",$', VERSION)
-update_version('pyproject.toml', '^version = "[0-9]+\.[0-9]+\.[0-9]+"$',  VERSION)
-update_version('docker/Dockerfile', '^.*museum_map-[0-9]+\.[0-9]+\.[0-9]+-py3.*$', VERSION)
+update_version(
+    "museum_map/server/frontend/package.json", r'(^  "version": ")[0-9]+\.[0-9]+\.[0-9]+(?:b[0-9]+)?(",$)', VERSION
+)
+update_version("museum_map/__about__.py", r'(^__version__ = ")[0-9]+\.[0-9]+\.[0-9]+(?:b[0-9]+)?("$)', VERSION)
+update_version(
+    "museum_map/server/frontend/src/store/config.ts", r'^(.*)[0-9]+\.[0-9]+\.[0-9]+(?:b[0-9]+)?(.*$)', VERSION
+)
+update_version("CHANGELOG.md", r"(## )Dev($)", VERSION)
