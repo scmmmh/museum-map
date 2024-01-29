6b24ce9fdae0c98d7e835544f42574a145cb5150
Mark Hall <mark.hall@work.room3b.eu>
2024-01-29 16:54:09 +0000

    Anonymise the data further on the client side

--- museum_map/server/frontend/src/store/tracking.ts
+++ museum_map/server/frontend/src/store/tracking.ts
@@ -65,13 +65,24 @@ export function track(action: LogAction) {
  */
 function sendTracking() {
   if (userId !== null) {
+    const cleaned = trackLog.map((item) => {
+      if (item.params && item.params.term) {
+        item.params.term = item
+          .params
+          .term
+          .replace(/[\w-\.]+@([\w-\.]+)?/g, 'email@domain.masked')
+          .replace(/[0-9\-]{8,19}/g, '0000-0000-0000-0000');
+        console.log(item.params.term);
+      }
+      return item;
+    });
+    trackLog = [];
     window.fetch('/api/tracking/track/' + userId, {
       method: 'POST',
-      body: JSON.stringify(trackLog),
+      body: JSON.stringify(cleaned),
       headers: { 'Content-Type': 'application/json' },
       keepalive: true,
     });
-    trackLog = [];
   }
 }
 
