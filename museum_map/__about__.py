c109dc39ec1e50409c8076658cbde08bcd31bf4c
Mark Hall <mark.hall@work.room3b.eu>
2024-02-02 14:37:53 +0000

    v1.0.1

--- museum_map/__about__.py
+++ museum_map/__about__.py
@@ -1,2 +1,2 @@
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __tables__ = {"groups", "users", "floors_items", "floor_topics", "rooms", "floors", "log_entries", "items"}
--- prepare_release.py
+++ prepare_release.py
@@ -2,7 +2,7 @@
 
 import re
 
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 
 def readlines(filename: str) -> list[str]:
