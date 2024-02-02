51a8f4a1fa4fc0268c149e063c4a1dd45c08ea38
Mark Hall <mark.hall@work.room3b.eu>
2024-02-02 14:07:20 +0000

    Re-added the not-ready display

--- museum_map/server/frontend/src/Root.svelte
+++ museum_map/server/frontend/src/Root.svelte
@@ -9,6 +9,7 @@
   import Research from "./components/Research.svelte";
   import Reload from "./components/Reload.svelte";
   import Demographics from "./routes/Demographics.svelte";
+  import NotReady from "./components/NotReady.svelte";
   import { apiRequest } from "./util";
 
   let Floor = null;
@@ -21,31 +22,28 @@
     queryFn: apiRequest<APIStatus>,
     refetchInterval: 60000,
   });
-
-  const reloadRequired = derived(apiStatus, (apiStatus) => {
-    if (apiStatus.isSuccess && apiStatus.data.version !== "0.9.1") {
-      return true;
-    }
-    return false;
-  });
 </script>
 
 <div class="bg-neutral-600 min-h-screen">
   {#if $apiStatus.isSuccess}
-    <main
-      class="container mx-auto bg-neutral-700 text-white shadow-lg shadow-black font-serif tracking-default"
-    >
-      <Route path="/"><Lobby /></Route>
-      <Route path="/floor/:fid"
-        >{#if Floor !== null}<svelte:component this={Floor} />{/if}</Route
+    {#if $apiStatus.data.ready}
+      <main
+        class="container mx-auto bg-neutral-700 text-white shadow-lg shadow-black font-serif tracking-default"
       >
-      <Route path="/floor/:fid/room/:rid/*"><Room /></Route>
-      <Route path="/demographics"><Demographics /></Route>
-      <NotFoundRoute><Lobby /></NotFoundRoute>
-    </main>
-    <Research />
-    {#if $reloadRequired}
-      <Reload />
+        <Route path="/"><Lobby /></Route>
+        <Route path="/floor/:fid"
+          >{#if Floor !== null}<svelte:component this={Floor} />{/if}</Route
+        >
+        <Route path="/floor/:fid/room/:rid/*"><Room /></Route>
+        <Route path="/demographics"><Demographics /></Route>
+        <NotFoundRoute><Lobby /></NotFoundRoute>
+      </main>
+      <Research />
+      {#if $apiStatus.data.version !== "0.9.1"}
+        <Reload />
+      {/if}
+    {:else}
+      <NotReady />
     {/if}
   {:else if $apiStatus.isLoading}
     <Loading />
---
+++ museum_map/server/frontend/src/components/NotReady.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+</script>
+
+<svelte:head>
+  <title>The Museum Map is currently not available</title>
+</svelte:head>
+
+<div
+  transition:fade
+  class="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 max-w-full p-4 bg-neutral-700 text-white shadow-2xl rounded-2xl"
+  role="alert"
+>
+  <p class="mb-2">Unfortuantely the Museum Map is currently not available.</p>
+  <p>
+    When the Museum Map becomes available, the interface will load
+    automatically.
+  </p>
+</div>
