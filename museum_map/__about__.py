383c08078edc7adf5793eaa89ca032935d72bc13
Mark Hall <mark.hall@work.room3b.eu>
2025-06-26 15:55:51 +0100

    v1.2.0b2

--- museum_map/__about__.py
+++ museum_map/__about__.py
@@ -1,4 +1,4 @@
 """About this package."""
 
-__version__ = "1.2.0b1"
+__version__ = "1.2.0b2"
 __tables__ = {"groups", "users", "floors_items", "floor_topics", "rooms", "floors", "log_entries", "items"}
